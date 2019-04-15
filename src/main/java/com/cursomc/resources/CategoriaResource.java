f3e3ec409901529e5177df4218d334ad377d9995
antonio <limaantoniocarlos20@gmail.com>
2019-04-14 21:52:03 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -2,8 +2,6 @@ package com.cursomc.resources;
 
 
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
